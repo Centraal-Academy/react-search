322e2dddb54365cf749ef46f8129fe7281252b5a
DezkaReid <elmaildeldezkareid@gmail.com>
2018-03-04 21:56:39 -0600

    initial setup app

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+class App extends React.Component {
+  render () {
+    return (
+      <section>
+        <h1>Search Course</h1>
+      </section>
+    )
+  }
+}
+
+export default App

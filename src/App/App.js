69cd39721aa12bdf2a489cd8a73be71b173afedd
DezkaReid <elmaildeldezkareid@gmail.com>
2018-03-04 22:10:58 -0600

    Search added to app

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import SearchInput from '../components/SearchInput/SearchInput'
 import './App.css'
 
 class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this._handleChange = this._handleChange.bind(this)
+    this._handleSubmit = this._handleSubmit.bind(this)
+  }
+
+  /* Handler when input in searchinput change */
+  _handleChange (event) {
+    console.log(event.target.value)
+  }
+
+  /* Avoid submit in form inside searchinput */
+  _handleSubmit (event) {
+    event.preventDefault()
+  }
+
   render () {
     return (
       <section>
         <h1>Search Course</h1>
+        <SearchInput onSubmit={this._handleSubmit} onChange={this._handleChange} />
       </section>
     )
   }

dbc792a6bd049b9fbcfc0726f20f106ec7459ec9
DezkaReid <elmaildeldezkareid@gmail.com>
2018-03-05 00:22:03 -0600

    Added method to get courses using Firebase API wrapper

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import SearchInput from '../components/SearchInput/SearchInput'
 import CourseList from '../components/CourseList/CourseList'
+import FirebaseAPI from '../modules/FirebaseAPI'
 import './App.css'
+
+const API_URL = 'https://searcher-academy.firebaseio.com/courses.json'
 
 class App extends React.Component {
   constructor (props) {
@@ -11,6 +14,20 @@ class App extends React.Component {
     }
     this._handleChange = this._handleChange.bind(this)
     this._handleSubmit = this._handleSubmit.bind(this)
+  }
+
+  componentDidMount () {
+    this.getCourses(API_URL)
+  }
+
+  getCourses (url, pattern) {
+    let query = ''
+    if (pattern) {
+      query = `?orderBy="name"&startAt="${pattern}"&endAt="${pattern}\uf8ff"`
+    }
+    const ENDPOINT_URL = `${url}${query}`
+    FirebaseAPI.get(ENDPOINT_URL)
+      .then(courses => this.setState({ courses }))
   }
 
   /* Handler when input in searchinput change */

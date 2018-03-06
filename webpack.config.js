1a905527d2df83d14d84a131829bcf42bac6fcaf
DezkaReid <elmaildeldezkareid@gmail.com>
2018-03-05 22:37:39 -0600

    Added copy webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const path = require('path')
 
 const config = {
@@ -23,7 +24,11 @@ const config = {
     new HtmlWebPackPlugin({
       template: path.resolve(__dirname, 'src/index.html'),
       filename: './index.html'
-    })
+    }),
+    new CopyWebpackPlugin([
+      {from: 'src/manifest.json'},
+      {from: 'src/sw.js'}
+    ])
   ]
 }
 

2d8e9361d6c71e2d9af611091ebebb5fe16ffc8f
DezkaReid <elmaildeldezkareid@gmail.com>
2018-03-05 23:14:42 -0600

    Added cache and fetch event

--- src/sw.js
+++ src/sw.js
@@ -1,8 +1,45 @@
-/* global self */
-self.addEventListener('install', (event) => {
-  console.log('Installed service worker')
+/* global self, caches, fetch */
+
+const MY_CACHE_NAME = 'search-cache-v1'
+
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/sw.js',
+  '/main.js'
+]
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(MY_CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache)
+      })
+  )
 })
 
 self.addEventListener('activate', (event) => {
-  console.log('Activated service worker')
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return cacheNames.filter(cacheName => cacheName !== MY_CACHE_NAME)
+      })
+      .then(cachesToDelete => {
+        const deletionPromises = cachesToDelete.map(cacheName => caches.delete(cacheName))
+        return Promise.all(deletionPromises)
+      })
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response
+        }
+        return fetch(event.request)
+      })
+  )
 })

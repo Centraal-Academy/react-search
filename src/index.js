3f2883062d75ec8a54b29127d085204affb785e2
DezkaReid <elmaildeldezkareid@gmail.com>
2018-03-05 22:41:28 -0600

    Added register worker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App/App'
+import RegisterWorker from './modules/RegisterWorker'
+
+window.addEventListener('load', event => {
+  RegisterWorker.register(window.navigator, './sw.js')
+    .then(console.log)
+    .catch(console.error)
+})
 
 const appContainer = document.getElementById('app')
 

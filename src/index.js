903f7077ec28afd9dd442ad637d809ac53233332
DezkaReid <elmaildeldezkareid@gmail.com>
2018-03-06 20:29:21 -0600

    Added request permission to show notification

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+/* global Notification */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App/App'
 import RegisterWorker from './modules/RegisterWorker'
 
 window.addEventListener('load', event => {
-  RegisterWorker.register(window.navigator, './sw.js')
+  RegisterWorker.register(navigator, './sw.js')
+    .then((registration) => {
+      if (Notification.permission === 'default') {
+        return Notification.requestPermission()
+      }
+      return true
+    })
+    .catch(console.error)
     .then(console.log)
     .catch(console.error)
 })

d72842661381e01a6e8a0cb07bcb4be26a7f101d
DezkaReid <elmaildeldezkareid@gmail.com>
2018-03-09 01:46:26 -0600

    removed firebase references and refactor using adapter

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
-/* global  */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App/App'
 import RegisterWorker from './modules/RegisterWorker'
-import firebase from 'firebase'
-
-function initFirebase (firebase) {
-  const config = {
-    apiKey: 'AIzaSyCY0cf61AFfz0qq_jho-rEe42nlHKaZjFc',
-    authDomain: 'searcher-academy.firebaseapp.com',
-    databaseURL: 'https://searcher-academy.firebaseio.com',
-    projectId: 'searcher-academy',
-    storageBucket: 'searcher-academy.appspot.com',
-    messagingSenderId: '703186646581'
-  }
-
-  firebase.initializeApp(config)
-}
+import FirebaseAdapter from './modules/FirebaseAdapter'
+const messaging = FirebaseAdapter.getMessaging()
 
 window.addEventListener('load', event => {
   RegisterWorker.register(navigator, './sw.js')
@@ -25,9 +12,6 @@ window.addEventListener('load', event => {
     })
     .catch(console.error)
 
-  initFirebase(firebase)
-
-  const messaging = firebase.messaging()
   messaging.requestPermission()
     .then(permission => messaging.getToken())
     .then(console.log)
